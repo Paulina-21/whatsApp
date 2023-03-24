4324281da7a601271019e75f06b544c30cc7e7ba
Niko D <nikodojan@live.de>
2023-03-24 21:17:57 +0100

    added call data and call page content

--- src/app/pages/calls/calls.page.ts
+++ src/app/pages/calls/calls.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import callsData from "../../../assets/data/calls.json"
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-calls',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calls.page.scss'],
 })
 export class CallsPage implements OnInit {
+  allCalls = callsData.sort((a,b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
+  calls = this.allCalls;
+  segmentFilter = "all";
+  @ViewChildren(IonItemSliding) items: QueryList<IonItemSliding>;
+  edit = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  segmentChanged(){
+    if (this.segmentFilter == "all"){
+      this.calls = this.allCalls;
+    } else {
+      this.calls = this.allCalls.filter(call=>call.missed);
+    }
+  }
+
+  toggleEdit(){
+    this.edit = !this.edit;
+
+    this.items.forEach(item=> {
+      if (this.edit) {
+        item.open('start');
+      } else {
+        item.close()
+      }
+    })
+  }
+
+  removeCall(call){
+    // for testing it's filter, needs a delete implementation wher it actually is removed
+    this.calls = this.calls.filter(c=>c.id != call.id);
   }
 
 }
