93faabeab4945264d03b182da91f72cf803a8e3b
Paula Catalina Curca <paul6795@edu.easj.dk>
2023-03-27 06:58:00 +0200

    Chats

--- src/app/pages/chats/chats.page.ts
+++ src/app/pages/chats/chats.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import chatData from "../../../assets/data/chats.json";
 
-@Component({
-  selector: 'app-chats',
-  templateUrl: './chats.page.html',
-  styleUrls: ['./chats.page.scss'],
-})
+  @Component({
+    selector: "app-chats",
+    templateUrl: "./chats.page.html",
+    styleUrls: ["./chats.page.scss"],
+  })
 export class ChatsPage implements OnInit {
+  chats = chatData;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  
+  startChat() {
+  // TODO
   }
-
 }
